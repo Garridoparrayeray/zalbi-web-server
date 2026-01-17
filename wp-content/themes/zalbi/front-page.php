ec75138aed584867fbd65d801fbc84f7bdb71583
Garridoparrayeray <garridoparrayeray@gmail.com>
2026-01-17 11:02:00 +0100

    Principales cambios en la pagina principal, a espera de corrección

--- wp-content/themes/zalbi/front-page.php
+++ wp-content/themes/zalbi/front-page.php
@@ -15,67 +15,68 @@ get_header(); ?>
         <a href="<?php echo home_url('/catalogo'); ?>" class="btn">Ver Catálogo Completo</a>
       </div>
     </section>
-
+  <section class="section-pad">
+      <div class="container">
+        <h1>¿QUIENES SOMOS?</h1>
+        <p>
+          <b>Somos Zalbi aisia eta abentura</b> una empresa dedicada al ocio y entretenimiento infantil con mas de <b>15 años</b> de experiencia trabajando en bizkaia y euskadi (también nos trasladamos fuera) que haremos tu celebración un evento increible. Siempre con la atenta mirada de nuestros monitore/as
+        </p>
+      </div>
+    </section>
     <section class="section-pad">
         <div class="container">
             <h2 class="text-center section-title">¿Qué estás buscando?</h2>
             
             <div class="options">
                 
-                <a href="<?php echo home_url('/catalogo/?cat=grandes'); ?>" class="options-box border-pink">
+                <a href="<?php echo home_url('/catalogo/?cat=hinchables'); ?>" class="options-box border-pink">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/hinchables-grandes.jpg" alt="Hinchables Grandes">
                     <div class="card-body">
-                        <h3 class="text-pink">Hinchables Grandes</h3>
-                        <p>Toboganes gigantes y pistas americanas para la máxima diversión.</p>
+                        <h3 class="text-pink">Hinchables</h3>
+                        <p>Disponemos de una gran variedad de castillos hinchables de distintos tamaños y para diferentes edades. Siempre bajo la supervisión de un monitor/a</p>
                         <span class="link-more text-pink">Ver más &rarr;</span>
                     </div>
                 </a>
 
-                <a href="<?php echo home_url('/catalogo/?cat=medianos'); ?>" class="options-box border-orange">
+                <a href="<?php echo home_url('/catalogo/?cat=acuaticos'); ?>" class="options-box border-orange">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/hinchables-medianos.jpg" alt="Hinchables Medianos"> 
                     <div class="card-body">
-                        <h3 class="text-orange">Hinchables Medianos</h3>
-                        <p>Ideales para cumpleaños y fiestas privadas en espacios reducidos.</p>
+                        <h3 class="text-orange">Fiesta de espuma e hinchables acuáticos</h3>
+                        <p>Para cuando el calor aprieta la mejor espuma e hinchables de agua</p>
                         <span class="link-more text-orange">Ver más &rarr;</span>
                     </div>
                 </a>
 
-                <a href="<?php echo home_url('/eventos'); ?>" class="options-box border-green">
+                <a href="<?php echo home_url('/catalogo/?=deportivos'); ?>" class="options-box border-green">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/eventos.jpg" alt="Eventos Especiales">
                     <div class="card-body">
-                        <h3 class="text-green">Eventos Especiales</h3>
+                        <h3 class="text-green">ATRACCIONES DEPORTIVAS</h3>
+                        <p>Atracciones deportivas e hinchables para pasar un buen rato, para la motricidad y como no, risas garantizadas.</p>
+                        <span class="link-more text-green">Ver más &rarr;</span>
+                    </div>
+                </a>
+                <a href="<?php echo home_url('/eventos')?>" class = "options-box border-green">
+                  <img src="" alt="<?php echo get_template_directory_uri(); ?>/img/eventos.jpg" alt="Eventos Especiales">
+                  <div class="card-body">
+                        <h3 class="text-green">EVENTOS</h3>
                         <p>Fiestas de la espuma, tirolinas y animación musical completa.</p>
                         <span class="link-more text-green">Ver más &rarr;</span>
                     </div>
                 </a>
-
             </div>
         </div>
     </section>
 
     <section class="section-pad">
-      <div class="container"> <div class="section-events">
-          <h2 style="color: var(--c-purple)">Últimos Eventos</h2>
-          <a href="#" style="color: var(--c-pink); font-weight: bold">Ver Galería Completa &rarr;</a>
-        </div>
-        
-        <div class="options">
-          <a href="#" class="event-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/evento1.jpg" alt="Fiesta Colegio" />
-            <h4 class="event-title">Fiesta Colegio San José</h4>
-          </a>
+        <p>Te organizamos vuestro evento adaptandonos a vuestro espacio y presupuesto
 
-          <a href="#" class="event-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/evento2.jpg" alt="Fiesta Ayuntamiento" />
-            <h4 class="event-title">Ayuntamiento de Bilbao</h4>
-          </a>
+        *AYUNTAMIENTOS Y COMISION DE FIESTAS*
 
-          <a href="#" class="event-card">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/evento3.jpg" alt="Cumpleaños" />
-            <h4 class="event-title">Cumpleaños Privado</h4>
-          </a>
-        </div>
-      </div>
+        Organizamos fiestas y eventos para vuestro pueblo bien a ayuntamientos o bien comisiones de fiesta.
+
+        *COLEGIOS Y CUALQUIER ASOCIACIÓN*
+        Celebramos fiestas para vuestra fiesta de fin de curso o cualqui3er evento que tengas, garantizando profesionalidad y diversión.
+        </p>
     </section>
 
 <?php get_footer(); ?>
